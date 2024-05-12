1f9807f1866d4af253fd4dd69fa689b76ada9664
OlivierCodes <datasciences1010@gmail.com>
2024-05-12 10:50:15 +0000

    Ajoutez des fichiers projet.

---
+++ MAUI_NavigationDrawer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_NavigationDrawer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI_NavigationDrawer/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MAUI_NavigationDrawer
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+
+}
